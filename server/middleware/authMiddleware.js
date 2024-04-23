364b8500dc2feb69318645c5b068507f0e6e864a
DipeshRaval <dipeshraval0007@gamil.com>
2024-04-23 22:36:24 +0530

    add test

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,8 @@ const { Admin } = require("../models");
 const authValidate = (req, res, next) => {
     const token = req.headers["authorization"];
     if (token == null) return res.sendStatus(401);
-    jwt.verify(token.split(" ")[1], process.env.MY_SECRET_KEY, async (err, user) => {
+    jwt.verify(token.split(" ")[1], "Drvl@TestCase", async (err, user) => {
+        // jwt.verify(token.split(" ")[1], process.env.MY_SECRET_KEY, async (err, user) => {
         if (err) return res.sendStatus(403);
         const u = await Admin.findByPk(user.id);
         req.user = u;
