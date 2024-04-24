fd20217aaf6206403ace93bfda4b846f2405877c
DipeshRaval <dipeshraval0007@gamil.com>
2024-04-24 17:20:00 +0530

    test slack

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m96ms2o73ao"
+    "revision": "0.nmfiqfg4ino"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
